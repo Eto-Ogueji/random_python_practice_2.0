e40cddd32e0365d95e016bada44969c9a1516dee
EtoOgueji <etoogueji@gmail.com>
2024-01-11 23:39:47 +0100

    2

---
+++ main2.py
@@ -0,0 +1,24 @@
+def f(m):
+    m.append(3)
+
+
+list3 = [1, 2]
+
+f(list3)
+
+print(list3)
+
+"""
+Examples of immutable Data Types:
+• int, long, float, complex
+• str
+• bytes
+• tuple
+• frozenset
+
+Examples of mutable Data Types:
+• bytearray
+• list
+• set
+• dict
+"""
