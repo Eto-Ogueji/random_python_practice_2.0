35ec1a73d5ab7c5b419f093c88186b65bd3d4318
EtoOgueji <etoogueji@gmail.com>
2024-01-11 23:39:18 +0100

    dict prac

---
+++ defaultdict.py
@@ -0,0 +1,8 @@
+from collections import defaultdict
+state_capitals = defaultdict(lambda: 'Boston')
+
+state_capitals['Bauchi'] = 'Bauchi'
+state_capitals['Abia'] = 'Umuiaha'
+state_capitals['Abuja'] = 'FCT'
+
+state_capitals['Bayelsa']
