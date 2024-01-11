1afa77f6a5ae6be28ab5388e04617517224453d2
EtoOgueji <etoogueji@gmail.com>
2024-01-11 23:40:24 +0100

    4

---
+++ main4.py
@@ -0,0 +1,12 @@
+familyMembers = {
+    'Father': 'Etomchukwu Oguejiofor',
+    'Mother': 'Ijeoma Oguejiofor',
+    'Son': 'Etomchukwu Oguejiofor II',
+    'Daughter': 'Ivy Oguejiofor',
+}
+
+for i in familyMembers.keys():
+    print('Hello {} you\'re the {}'.format(familyMembers[i], i))
+
+
+print(familyMembers['Son'])
